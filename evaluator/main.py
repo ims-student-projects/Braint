445dec2b8559ca1a65eb008a76569f2b93897ac1
vgratian <vgratian@utopianlab.am>
2018-04-25 01:29:18 +0200

    updated scores and main, removed result

--- evaluator/main.py
+++ evaluator/main.py
@@ -1,13 +1,14 @@
-import corpus
+from corpus import Corpus
+from scores import Scores
 
 def main():
-    file_tweets = "/Users/marina/Documents/Master/2018_SS/TeamLab/iest/trial.csv"
-    file_labels = "/Users/marina/Documents/Master/2018_SS/TeamLab/iest/trial.labels"
-    corpus = Corpus.Corpus(file_tweets, file_labels)
-    print(next(corpus))
-    print(next(corpus))
-    print(next(corpus))
 
+    gold = '../data/trial.labels' # file contains gold labels
+    pred_with_tweets = '../data/trial.csv' # predicted labels + tweet text
+
+    mycorpus = Corpus(pred_with_tweets, gold)
+    myscores = Scores(mycorpus)
+    print(myscores)
 
 if __name__ == "__main__":
     main()
--- evaluator/scores.py
+++ evaluator/scores.py
@@ -1,53 +1,62 @@
+from time import time
 
 class Scores():
 
     def __init__(self, iterator):
+        start = time() # used to count runtime
+
         self.tweets = iterator
-        self.emotions = ['surprise', 'disgust', 'fear', 'sad', 'joy', 'anger']
+        self.labels = ['surprise', 'disgust', 'fear', 'sad', 'joy', 'anger']
 
         # Initiate base scores with values set to 0
-        self.true_positives = {x:0 for x in self.emotions}
-        self.false_positives = {x:0 for x in self.emotions}
-        self.false_negatives = {x:0 for x in self.emotions}
+        self.true_positives = {x:0 for x in self.labels}
+        self.false_positives = {x:0 for x in self.labels}
+        self.false_negatives = {x:0 for x in self.labels}
 
         # Calculate the base scores
         self.get_base_scores()
 
         # F-score for each class in a dict
-        self.f_all = {x:self.get_f_score(x) for x in self.emotions}
+        self.f_all = {x:self.get_f_score(x) for x in self.labels}
 
         # Micro and Macro F-scores
         self.f_micro = self.get_micro()
         self.f_macro = self.get_macro()
 
+        # Other
+        self.runtime = round(time()-start, 3)
 
-    def get_base_scores():
+
+    def get_base_scores(self):
         """
         Calculate TP, FP and FN scores from the tweets.
         """
         for tweet in self.tweets:
-            for emotion in self.emotions:
-                if tweet.get_pred_label() == emotion:
-                    if tweet.get_gold_label() == emotion:
-                        true_positives[emotion] += 1
+            for label in self.labels:
+                if tweet.get_pred_label() == label:
+                    if tweet.get_gold_label() == label:
+                        self.true_positives[label] += 1
                     else:
-                        false_positives[emotion] += 1
-                elif tweet.get_gold_label() == emotion:
-                    fasel_negatives[emotion] += 1
+                        self.false_positives[label] += 1
+                elif tweet.get_gold_label() == label:
+                    self.false_negatives[label] += 1
 
 
-    def get_f_score(emotion):
-        tp = self.true_positives[emotion]
-        fp = self.false_positives[emotion]
-        fn = self.false_negatives[emotion]
+    def get_f_score(self, label):
+        tp = self.true_positives[label]
+        fp = self.false_positives[label]
+        fn = self.false_negatives[label]
 
-        precision = tp / (tp + fp)
-        recall = tp / (tp + fn)
+        precision = tp / (tp+fp) if (tp+fp)!=0 else 0
+        recall = tp / (tp + fn) if (tp+fn)!=0 else 0
 
-        return (2 * precision * recall ) / (precision + recall)
+        if (precision+recall) == 0:
+            return 0.0
+
+        return (2*precision*recall) / (precision+recall)
 
 
-    def get_micro():
+    def get_micro(self):
         tp = sum(self.true_positives.values())
         fp = sum(self.false_positives.values())
         fn = sum(self.false_negatives.values())
@@ -57,11 +66,12 @@ class Scores():
 
         return (2 * precision * recall ) / (precision + recall)
 
-
-    def get_macro():
+    def get_macro(self):
         return sum(self.f_all.values()) / len(self.f_all)
 
 
-class Result():
-    def __init__(self, scores):
-        pass
+    def __str__(self):
+        text = 'Evaluation completed in {} seconds.\n' \
+            'Macro F-score: {}. Micro F-score: {}'.format(self.runtime,
+            round(self.f_macro, 3), round(self.f_micro, 3))
+        return text
