a8b298bda9df187b8ec848395962860c729587a6
haidmarina <haidmarina@gmail.com>
2018-04-25 08:12:44 +0200

    added commets for corpus and tweet

--- evaluator/corpus.py
+++ evaluator/corpus.py
@@ -3,8 +3,28 @@ from itertools import zip_longest
 import tweet
 
 class Corpus(object):
+    """ A datastructure to store Tweet objects.
+
+        The files from which the data to store in Tweet/Corpus datastructure is read in
+        is assumed to have the following format:
+
+            The file containg the tweets: each line consists of two tab separated columns, 
+                                          in the first column the predicted label for this tweet 
+                                          and in the second the text of this tweet.
+            
+            The file containg the gold labels: one label on each line no empty lines in between.
+
+            Both files have the same length and the lines in the files correspond to each other
+            (e.g. The first line of the gold label file contains the label for the tweet on the first line of the other file).
+    """
 
     def __init__(self, filename_tweets=None, filename_gold_labels=None):
+        """ Inits the Corpus.
+
+            Args:
+                (optional) filename_tweets: the name of the file containing the tweets and predicted labels.
+                (optional) filename_gold_labels: the name of the file containg the gold labels.
+        """
         self.__corpus = []
         self.__curr = 0 # counter for iterator
         if filename_tweets and filename_gold_labels:
@@ -20,7 +40,8 @@ class Corpus(object):
             self.__curr += 1
             return self.get_ith(self.__curr - 1)
 
-    def read_files(self, filename_tweets, filename_gold_labels):
+    def __read_files(self, filename_tweets, filename_gold_labels):
+        #TO DO: add some exception handling?
         with open (filename_tweets, 'r') as tweet_file, \
             open (filename_gold_labels) as gold_label_file:
             for tweet, gold_label in zip_longest(tweet_file, gold_label_file):
@@ -33,13 +54,29 @@ class Corpus(object):
                 self.append(tweet_obj)
 
     def length(self):
+        """ Gets the number of tweets in the corpus.
+
+            Returns:
+                The number of tweets storde in this corpus.
+        """
         return len(self.__corpus)
 
     def get_ith(self, i):
+        """ Gets the ith tweet in this corpus.
+
+            Return:
+                A tweet object.
+        """
         return self.__corpus[i]
 
-    # @override
     def append(self, tweet_obj, pos=None):
+        """ Adds an tweet object to this corpus. 
+            Unless otherwise specified the tweets is appended at the end.
+
+            Args:
+                tweet_obj: The tweet object to be added to this corpus.
+                (optional) pos: The position at which the element should be inserted.
+        """
         if pos is not None:
             self.__corpus.insert(pos, tweet_obj)
         else:
