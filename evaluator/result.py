4702247a16fc6cd084db80be69cbaaffc8800009
imandes0 <vgratian@utopianlab.am>
2018-05-04 22:13:56 +0200

    adding class Result

---
+++ evaluator/result.py
@@ -0,0 +1,16 @@
+
+class Result():
+
+    def __init__(self):
+        self.print_header()
+
+
+    def print_header(self):
+        bold = '\033[1m'
+        unbold = '\033[0m'
+        header = '{}Fmac\tFmic{}\tsupP\tsupR\tdisP\tdisR\tfeaP\tfeaR\tsadP\tsadR\tjoyP\tjoyR\tangP\tangR\n'.format(bold, unbold)
+        print(header)
+
+
+    def show(self, score):
+        print(score)
