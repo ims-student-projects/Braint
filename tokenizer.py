12577c7956f1ae0765f4d5659f39562e4f11a7a9
kiflom Desta Abrha <abrhaka@ims.uni-stuttgart.de>
2018-05-09 16:20:22 +0200

    :

--- tokenizer.py
+++ tokenizer.py
@@ -1,12 +1,44 @@
+import string
 
 class Tokenizer():
-
-    def __init__(self, text):
-        self.text = text
-
-    def get_tokens(self):
-        return self.text.split()
-
-    def get_terms(self):
-        unique = set(self.get_tokens())
-        return list(unique)
+    def __init__(self):
+        self.punct = string.punctuation
+        
+    def get_tokens(self, text):
+        tokens = text.split()
+        result = []
+        for token in tokens:
+            if token.isalpha():
+                result.append(token)
+            else: 
+                new_token=""
+                for char in token:
+                    if char in self.punct:
+                        result.append(new_token)
+                        result.append(char)
+                        new_token=""
+                    else :
+                        new_token +=char
+            if new_token:
+                result.append(new_token)
+        return result
+                        
+    def get_terms(self, text):
+        p =['~', '!', '#', '$', '%', '^', '&', '*', ',', '?']
+        newStr =''
+        i =0
+        punc = None
+        for i in range(len(text)):
+            if text[i] in p:
+                newStr = ' '
+                newStr = text[i]
+                punc =False
+            else:
+                newStr = text[i]
+                punc = True 
+        return newStr.split('  ')
+    
+if __name__ == '__main__':
+    a = Tokenizer()
+    b = a.get_tokens("hello, world!")
+    print(b)
