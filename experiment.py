702d6fa3ce6d3aaa7e772f29107020ae2de7d1d6
haidmarina <haidmarina@gmail.com>
2018-05-15 14:20:19 +0200

    fixed missing paranthesis

--- experiment.py
+++ experiment.py
@@ -1,8 +1,8 @@
 from Evaluator.scorer import Scorer
 from Evaluator.result import Result
 from corpus import Corpus
-#from featurer import Featurer
-from baseline_feature_extractor import FeatureExtractor
+from featurer import Featurer
+#from baseline_feature_extractor import FeatureExtractor
 from multiclass_perceptron import MulticlassPerceptron
 
 def main():
@@ -24,14 +24,19 @@ def main():
     test_corpus = Corpus(test_tweets, gold_labels)
 
     # Extract features
-    feature_extractor = FeatureExtractor()
-    feature_extractor.extract_features(train_corpus)
-    feature_extractor.extract_features(test_corpus)
+    #feature_extractor = FeatureExtractor()
+    #feature_extractor.extract_features(train_corpus)
+    #feature_extractor.extract_features(test_corpus)
+    feature_extractor = Featurer(train_corpus)
+    feature_extractor.set_features()
+    #for tweet in train_corpus:
+    #    print(tweet.get_features())
+    feat_ex = Featurer(test_corpus)
+    feat_ex.set_features()
 
     # Train classifier
     classifier = MulticlassPerceptron(classes, train_corpus.get_all_feature_names())
-    classifier.train(5, train_corpus)
-
+    classifier.train(1, train_corpus)
     # Test classifier
     classifier.test(test_corpus)
 
