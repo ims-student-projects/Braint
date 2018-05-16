cb23a75c7542214d3d186fc2e00a0652b0103c15
imandes0 <vgratian@utopianlab.am>
2018-05-16 11:52:27 +0200

    fixing bugs

--- tokenizer.py
+++ tokenizer.py
@@ -3,42 +3,27 @@ import string
 class Tokenizer():
     def __init__(self):
         self.punct = string.punctuation
-        
+
     def get_tokens(self, text):
         tokens = text.split()
         result = []
         for token in tokens:
             if token.isalpha():
-                result.append(token)
-            else: 
+                result.append(token.lower())
+            else:
                 new_token=""
                 for char in token:
                     if char in self.punct:
-                        result.append(new_token)
+                        result.append(new_token.lower())
                         result.append(char)
                         new_token=""
                     else :
                         new_token +=char
-            if new_token:
-                result.append(new_token)
+                if new_token:
+                    result.append(new_token.lower())
         return result
-                        
+
+
     def get_terms(self, text):
-        p =['~', '!', '#', '$', '%', '^', '&', '*', ',', '?','*']
-        newStr =''
-        i =0
-        punc = None
-        for i in range(len(text)):
-            if text[i] in p:
-                newStr = ' '
-                newStr = text[i]
-                punc =False
-            else:
-                newStr = text[i]
-                punc = True 
-        return newStr.split('  ')
-    
-if __name__ == '__main__':
-    a = Tokenizer()
-    b = a.get_tokens("hello, world!")
-    print(b)
+        unique = set(self.get_tokens(text))
+        return list(unique)
