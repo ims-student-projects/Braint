12b9bef6ff3a370333ec68868440429ac645a8b5
imandes0 <vgratian@utopianlab.am>
2018-05-23 07:07:02 +0200

    running the experiment on more options

--- experiment.py
+++ experiment.py
@@ -17,25 +17,29 @@ def main():
     test_corpus = Corpus(gold_labels)
 
     # Extract features
-    features_train = Featurer(train_corpus, 5)
-    features_test = Featurer(test_corpus, 5)
-    types = [ 'binary', 'count', 'frequency', 'tf-idf']
-    for type in types:
-        print('Extracting features -- {}'.format(type))
-        features_train.extract(type)
-        features_test.extract(type)
-        result = Result()
+    perc = [0, 5, 10, 20]
+    iterations = [1, 2, 5, 10, 30]
+    for p in perc:
+        print('Choosing stopwords threshold --> {}'.format(p))
+        features_train = Featurer(train_corpus, 5)
+        features_test = Featurer(test_corpus, 5)
+        types = [ 'binary', 'count', 'frequency', 'tf-idf']
+        for type in types:
+            print('Extracting features --> {}'.format(type))
+            features_train.extract(type)
+            features_test.extract(type)
+            result = Result()
 
-        # Train and test model
-        for i in range(1,10):
-            # train
-            classifier = MulticlassPerceptron(classes, train_corpus.get_all_feature_names())
-            classifier.train(i, train_corpus)
-            # test
-            classifier.test(test_corpus)
-            # evaluate and show result
-            scores = Scorer(test_corpus)
-            result.show(scores)
+            # Train and test model
+            for i in iterations:
+                # train
+                classifier = MulticlassPerceptron(classes, train_corpus.get_all_feature_names())
+                classifier.train(i, train_corpus)
+                # test
+                classifier.test(test_corpus)
+                # evaluate and show result
+                scores = Scorer(test_corpus)
+                result.show(scores)
 
 if __name__ == "__main__":
     main()
