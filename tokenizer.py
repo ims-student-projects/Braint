5b8e24c222b69eb17beb07e4fec84a0c9fa134a6
imandes0 <vgratian@utopianlab.am>
2018-06-12 15:48:17 +0200

    adding emoji/emoticon recognition, adding labels to tokens

--- tokenizer.py
+++ tokenizer.py
@@ -1,33 +1,83 @@
-import string
+import string, re, emoji
+from utils.translate_emoticon import emoticon_to_label
+
 
 class Tokenizer():
     def __init__(self):
         self.punct = string.punctuation
 
-    def get_tokens(self, text):
+    def get_tokens(self, text, \
+            lowercase = False,
+            replace_emojis = False,
+            remove_stopword = False,
+            remove_punct = False ):
+
+        # Convert to lowercase
+        if lowercase:
+            text = text.lower()
+
+        # Codify emoticons as tokens
+        for emoticon in emoticon_to_label.keys():
+            if replace_emojis:
+                # Replace with labels (eg ":-)" > "[#joy#]")
+                replace_by = '[#{}#]'.format(emoticon_to_label[emoticon][1:-1])
+            else:
+                replace_by = '[#{}#]'.format(emoticon)
+            text = re.sub(emoticon, replace_by, text)
+
         tokens = text.split()
         result = []
+
         for token in tokens:
-            if token.isalpha() or token == '[#TRIGGERWORD#]' or token == 'http://url.removed':
-                result.append(token)
-            elif token != '[NEWLINE]': #ignote this newline thing
-                new_token=""
+            # Token is word
+            if token.isalpha():
+                result.append({'token':token, 'label':'word'})
+            # Token is special token
+            elif re.fullmatch('^\[#(.*)#\]$', token):
+                if token.lower() == '[#triggerword#]':
+                    result.append({'token':token, 'label':'triggerword'})
+                else:
+                    result.append({'token':token[2:-2], 'label':'emoticon'})
+            # Token is removed url
+            elif token.lower() == 'http://url.removed':
+                result.append({'token':'[#URL#]', 'label':'url'})
+
+            elif token.lower() != '[newline]': # ignore newline symbol
+
+            # Look for punctuation, emojis or whitespace and split
+                new_token = ''
                 for char in token:
+                    # Character is punctuation
                     if char in self.punct:
-                        result.append(char)
-                        if new_token: # only add if not empty!
-                            result.append(new_token)
-                        new_token=""
+                        if not remove_punct:
+                            result.append({'token':char, 'label':'punctuation'})
+                        # add preceding charachters to results, if any
+                        if new_token:
+                            result.append({'token':new_token, 'label':'word'})
+                            new_token = ''
+                    # Character is emoji
+                    elif char in emoji.UNICODE_EMOJI.keys():
+                        if replace_emojis:
+                            emoji_text = emoji.demojize(char) # ignore columns
+                            result.append({'token': emoji_text[1:-1], 'label': 'emoji'})
+                        else:
+                            result.append({'token':char, 'label':'emoji'})
+                        # add preceding charachters to results, if any
+                        if new_token:
+                            result.append({'token':new_token, 'label':'word'})
+                            new_token = ''
+                    # Character is alpha-numerical
                     else:
-                        new_token +=char
+                        new_token += char
                 if new_token:
-                    result.append(new_token)
+                    result.append({'token':new_token, 'label':'word'})
         return result
+
 
     def remove_stopwords(self, word_list):
         processed_word_list = []
         for word in word_list:
-            word = word.lower() # in case they arenot all lower cased
+            #word = word.lower() # in case they arenot all lower cased
             # TODO if all are capital keep as itis and stopwords
             """if word not in stopwords.words("english"):
                 processed_word_list.append(word)
@@ -35,12 +85,16 @@ class Tokenizer():
             processed_word_list.append(word)
         return  processed_word_list
 
+
     def get_terms(self, text):
         unique = set(self.get_tokens(text))
         return list(unique)
 
+
 if __name__ == '__main__':
-    a = Tokenizer()
-    b = a.get_tokens("HeLlO\t,  WoRld!") # returned empty strings in previous version
-    c = a.remove_stopwords(b)
-    print(c)
+    tokenizer = Tokenizer()
+    test_sentence = 'HeLlO\t,  WoRld! <33333 :)))) [NEWLINE] >:\ 🤠 🙂 😃😄😆😍'
+    tokens = tokenizer.get_tokens(test_sentence, lowercase=True, remove_punct=True)
+    print(tokens, '\n')
+    print("Number of tokens: {}. Raw tokens: \n".format(len(tokens)))
+    print('"{}"\n'.format('" "'.join(t['token'] for t in tokens)))
---
+++ utils/translate_emoticon.py
@@ -0,0 +1,142 @@
+
+"""
+Emoticons to label mapper. List of emoticons is based on a list by EmojiOne
+Labs (https://demos.emojione.com/ascii-smileys.html). The emoticons are in
+regex representation, whereby potentially repeatable symbols are taken into
+account, i.e. ":)" and ":))))))" both map to the same label "joy".
+
+"""
+
+emoticon_to_label = {
+    r"<3+" : ":heart:",    # <3
+    r"</3+" : ":broken_heart:",    # </3
+    r":'\)+" : ":joy:",    # :')
+    r":'-\)+" : ":joy:",    # :'-)
+    r":D" : ":smiley:",    # :D
+    r":-D" : ":smiley:",    # :-D
+    r"=d" : ":smiley:",    # =d
+    r":\)+" : ":slight_smile:",    # :)
+    r":-\)+" : ":slight_smile:",    # :-)
+    r"=]" : ":slight_smile:",    # =]
+    r"=\)+" : ":slight_smile:",    # =)
+    r":]" : ":slight_smile:",    # :]
+    r"':\)+" : ":sweat_smile:",    # ':)
+    r"':-\)+" : ":sweat_smile:",    # ':-)
+    r"'=\)+" : ":sweat_smile:",    # '=)
+    r"':D" : ":sweat_smile:",    # ':D
+    r"':-D" : ":sweat_smile:",    # ':-D
+    r"'=D" : ":sweat_smile:",    # '=D
+    r">:\)+" : ":laughing:",    # >:)
+    r">;\)+" : ":laughing:",    # >;)
+    r">:-\)+" : ":laughing:",    # >:-)
+    r">=\)+" : ":laughing:",    # >=)
+    r";\)+" : ":wink:",    # ;)
+    r";-\)+" : ":wink:",    # ;-)
+    r"\*-\)+" : ":wink:",    # *-)
+    r"\*\)+" : ":wink:",    # *)
+    r";-]" : ":wink:",    # ;-]
+    r";]" : ":wink:",    # ;]
+    r";D" : ":wink:",    # ;D
+    r";^\)+" : ":wink:",    # ;^)
+    r"':\(" : ":sweat:",    # ':(
+    r"':-\(" : ":sweat:",    # ':-(
+    r"'=\(" : ":sweat:",    # '=(
+    r":\*" : ":kissing_heart:",    # :*
+    r":-\*" : ":kissing_heart:",    # :-*
+    r"=\*" : ":kissing_heart:",    # =*
+    r":^\*" : ":kissing_heart:",    # :^*
+    r">:P" : ":stuck_out_tongue_winking_eye:",    # >:P
+    r"X-P" : ":stuck_out_tongue_winking_eye:",    # X-P
+    r"x-p" : ":stuck_out_tongue_winking_eye:",    # x-p
+    r">:\[" : ":disappointed:",    # >:[
+    r":-\(" : ":disappointed:",    # :-(
+    r":\(" : ":disappointed:",    # :(
+    r":-\[" : ":disappointed:",    # :-[
+    r":\[" : ":disappointed:",    # :[
+    r"=\(\)+" : ":disappointed:",    # =()
+    r">:\(" : ":angry:",    # >:(
+    r">:-\(" : ":angry:",    # >:-(
+    r":@" : ":angry:",    # :@
+    r":'\(" : ":cry:",    # :'(
+    r":'-\(" : ":cry:",    # :'-(
+    r";\(" : ":cry:",    # ;(
+    r";-\(" : ":cry:",    # ;-(
+    r">.<" : ":persevere:",    # >.<
+    r"D:" : ":fearful:",    # D:
+    r":$" : ":flushed:",    # :$
+    r"=$" : ":flushed:",    # =$
+    r"#-\)+" : ":dizzy_face:",    # #-)
+    r"#\)+" : ":dizzy_face:",    # #)
+    r"%-\)+" : ":dizzy_face:",    # %-)
+    r"%\)+" : ":dizzy_face:",    # %)
+    r"X\)+" : ":dizzy_face:",    # X)
+    r"X-\)+" : ":dizzy_face:",    # X-)
+    r"\*\0/*" : ":ok_woman:",    # *\0/*
+    r"\0/" : ":ok_woman:",    # \0/
+    r"\*\\O/\*" : ":ok_woman:",    # *\O/*
+    r"\\O/" : ":ok_woman:",    # \O/
+    r"O:-\)+" : ":innocent:",    # O:-)
+    r"0:-3" : ":innocent:",    # 0:-3
+    r"0:3" : ":innocent:",    # 0:3
+    r"0:-\)+" : ":innocent:",    # 0:-)
+    r"0:\)+" : ":innocent:",    # 0:)
+    r"0;^\)+" : ":innocent:",    # 0;^)
+    r"O:-\)+" : ":innocent:",    # O:-)
+    r"O:\)+" : ":innocent:",    # O:)
+    r"O;-\)+" : ":innocent:",    # O;-)
+    r"O=\)+" : ":innocent:",    # O=)
+    r"0;-\)+" : ":innocent:",    # 0;-)
+    r"O:-3" : ":innocent:",    # O:-3
+    r"O:3" : ":innocent:",    # O:3
+    r"B-\)+" : ":sunglasses:",    # B-)
+    r"B\)+" : ":sunglasses:",    # B)
+    r"8\)+" : ":sunglasses:",    # 8)
+    r"8-\)+" : ":sunglasses:",    # 8-)
+    r"B-D" : ":sunglasses:",    # B-D
+    r"8-D" : ":sunglasses:",    # 8-D
+    r"-_-" : ":expressionless:",    # -_-
+    r"-__-" : ":expressionless:",    # -__-
+    r"-___-" : ":expressionless:",    # -___-
+    r">:\\" : ":confused:",    # >:\
+    r":P" : ":stuck_out_tongue:",    # >:/
+    r":-P" : ":stuck_out_tongue:",    # :-/
+    r"=p" : ":stuck_out_tongue:",    # :-.
+    r":-p" : ":stuck_out_tongue:",    # :/
+    r":p" : ":stuck_out_tongue:",    # :\
+    r"=p" : ":stuck_out_tongue:",    # =/
+    r":-Þ" : ":stuck_out_tongue:",    # =\
+    r":Þ" : ":stuck_out_tongue:",    # :L
+    r":þ" : ":stuck_out_tongue:",    # =l
+    r":-þ" : ":stuck_out_tongue:",    # :P
+    r":-b" : ":stuck_out_tongue:",    # :-P
+    r":b" : ":stuck_out_tongue:",    # =p
+    r"d:" : ":stuck_out_tongue:",    # :-p
+    r":-O" : ":open_mouth:",    # :p
+    r":O" : ":open_mouth:",    # =p
+    r":-o" : ":open_mouth:",    # :-Þ
+    r":o" : ":open_mouth:",    # :Þ
+    r"O_O" : ":open_mouth:",    # :þ
+    r">:O" : ":open_mouth:",    # :-þ
+    r":-X" : ":no_mouth:",    # :-b
+    r":X" : ":no_mouth:",    # :b
+    r":-#" : ":no_mouth:",    # d:
+    r":#" : ":no_mouth:",    # :-O
+    r"=x" : ":no_mouth:",    # :O
+    r"=x" : ":no_mouth:",    # :-o
+    r":x" : ":no_mouth:",    # :o
+    r":-x" : ":no_mouth:",    # O_O
+    r"=#" : ":no_mouth:",    # >:O
+    ":-X" : ":no_mouth:",    # :-X
+    ":X" : ":no_mouth:",    # :X
+    ":-#" : ":no_mouth:",    # :-#
+    ":#" : ":no_mouth:",    # :#
+    "=X" : ":no_mouth:",    # =X
+    "=x" : ":no_mouth:",    # =x
+    ":x" : ":no_mouth:",    # :x
+    ":-x" : ":no_mouth:",    # :-x
+    "=#" : ":no_mouth:",    # =#
+        }
+
+
+def translate_emoticon(emoticon):
+    return emoticon_to_label[emoticon]
