132f60aeb47b6b5d8d06740414b7d03d48e02b0d
Marina Haid <marina@Marinas-Air.fritz.box>
2018-08-16 10:42:50 +0200

    added commandline params

--- utils/plot.py
+++ utils/plot.py
@@ -1,3 +1,15 @@
+"""
+A script to create and plot confusion matrices.
+Notes:
+    - One or multiple inputfiles, titles and classnames can be given. Multiple values for one parameter have to be seperated by semicolons (;) and placed in between quotes.
+        e.g. python3 plot.py -i "path/to/file1;path/to/file2" -t "model1;model2" -c "class1;class2;class3" -n 1
+             python3 plot.py -i path/to/file -t model1 -c "class1;class2;class3" -n 1
+    - If the value of -n is 1 the confusion matrix values are normalized otherwise not.
+    - The inputfiles are expected to be tab-seperated files, with the first line being the header, 
+      where the first column contains the true label and the second colum contains the predicted label.
+"""
+#!/usr/bin/env python3
+import sys, getopt
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,14 +46,40 @@ def plot_confusion_matrix(confusion_matrix, classes, title, normalize=False):
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
 
-def main():
-    predictions_file = "/Users/marina/Documents/Master/2018_SS/TeamLab/experiment_eval/predictions_lstm.csv"
-    title = "LSTM"
-    classes = ["joy", "anger", "sad", "surprise", "fear", "disgust"]
-    confusion_matrix = create_confusion_matrix(predictions_file)
-    plt.figure()
-    plot_confusion_matrix(confusion_matrix, classes, title, True)
-    plt.show()
+def main(argv):
+    inputfiles = ""
+    titles = ""
+    classes = ""
+    is_norm = 0
+    usage = "Usage: plot.py -i <inputfile> -t <title> -c <classes> -n <normalized>"
+
+    try:
+        opts, args = getopt.getopt(argv,"hi:t:c:n:",["ifiles=","titles=","classes=","norm="])
+    except getopt.GetoptError:
+        print(usage)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(usage)
+            sys.exit()
+        elif opt in ("-i", "--ifiles"):
+            inputfiles = arg
+        elif opt in ("-t", "--titles"):
+            title = arg
+        elif opt in ("-c", "--classes"):
+            classes = arg
+        elif opt in ("-n", "--norm"):
+            is_norm = arg
+
+    is_norm = True if is_norm == 1 else False
+    titles = [t for t in title.split(";")]
+    classes = [c for c in classes.split(";")]
+    conf_matrices = [create_confusion_matrix(f) for f in inputfiles.split(";")]
+    
+    for cm, title in itertools.zip_longest(conf_matrices, titles):
+        plt.figure()
+        plot_confusion_matrix(cm, classes, title, is_norm)
+        plt.show()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
