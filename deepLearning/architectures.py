9fa53088205badf98ac0080d6144037b1b68c67a
Marina Haid <marina@Marinas-Air.fritz.box>
2018-08-18 22:29:39 +0200

    cleanup

--- deepLearning/architectures.py
+++ deepLearning/architectures.py
@@ -22,10 +22,6 @@ class LSTM_Model(object):
         self.model = self.create_LSTM()
 
     def parse_params(self, params=None):
-        if 'attention' in params:
-            self.attention = params['attention']
-        else:
-            self.attention = False
         if 'max_len' in params:
             self.max_sequence_len = params['max_len']
         else:
@@ -48,50 +44,81 @@ class LSTM_Model(object):
             self.loss = 'categorical_crossentropy'
 
     def create_LSTM(self):
-        if self.attention:
-            # Define the model
-            inp = Input(shape=(self.max_sequence_len,))
-            if self.weights is not None:
-                emb = Embedding(self.input_dim + 1,
+        model = Sequential()
+        if self.weights is not None:
+            model.add(Embedding(self.input_dim + 1,
+                        self.embedding_dim,
+                        weights=[self.weights],
+                        input_length=self.max_sequence_len,
+                        trainable=True))
+        else:
+            model.add(Embedding(self.input_dim + 1,
+                        self.embedding_dim,
+                        input_length=self.max_sequence_len,
+                        trainable=True))
+        model.add(Dropout(self.dropout))
+        model.add(LSTM(self.lstm_dim))
+        model.add(Dropout(self.dropout))
+        model.add(Dense(self.output_dim, activation='softmax'))
+        model.compile(optimizer=self.optimizer, loss=self.loss,
+                  metrics=['accuracy'])
+        return model
+
+class LSTM_ATT_Model(object):
+    def __init__(self, vocab_size, embedding_dim, output_dim, weights=None, params=None):
+        self.input_dim = vocab_size
+        self.embedding_dim = embedding_dim
+        self.lstm_dim = self.embedding_dim
+        self.output_dim = output_dim
+        self.weights = weights
+        self.parse_params(params)
+        self.model = self.create_LSTM_ATT()
+
+    def parse_params(self, params=None):
+        if 'max_len' in params:
+            self.max_sequence_len = params['max_len']
+        else:
+            self.max_sequence_len = None
+        if 'dropout' in params:
+            self.dropout = params['dropout']
+        else:
+            self.dropout = .5
+        if 'trainable_embeddings' in params:
+            self.trainable = params['trainable_embeddings']
+        else:
+            self.trainable = True
+        if 'optimizer' in params:
+            self.optimizer = params['optimizer']
+        else:
+            self.optimizer = 'adam'
+        if 'loss' in params:
+            self.loss = params['loss']
+        else:
+            self.loss = 'categorical_crossentropy'
+
+    def create_LSTM_ATT(self):
+        inp = Input(shape=(self.max_sequence_len,))
+        if self.weights is not None:
+            emb = Embedding(self.input_dim + 1,
                         self.embedding_dim,
                         weights=[self.weights],
                         input_length=self.max_sequence_len,
                         trainable=True)(inp)
-            else:
-                emb = Embedding(self.input_dim + 1,
+        else:
+            emb = Embedding(self.input_dim + 1,
                         self.embedding_dim,
                         input_length=self.max_sequence_len,
                         trainable=True)(inp)
-            x = SpatialDropout1D(self.dropout)(emb)
-            x = LSTM(self.lstm_dim, return_sequences=True)(x)
-            x, attention = Attention()(x)
-            x = Dense(self.output_dim, activation="sigmoid")(x)
-            model = Model(inputs=inp, outputs=x)
-            model.compile(loss=self.loss,
+        x = SpatialDropout1D(self.dropout)(emb)
+        x = LSTM(self.lstm_dim, return_sequences=True)(x)
+        x, attention = Attention()(x)
+        x = Dense(self.output_dim, activation="sigmoid")(x)
+        model = Model(inputs=inp, outputs=x)
+        model.compile(loss=self.loss,
               optimizer=self.optimizer,
               metrics=['accuracy'])
-            self.attention_model = Model(inputs=inp, outputs=attention)
-            return model
-        else:
-            model = Sequential()
-            if self.weights is not None:
-                model.add(Embedding(self.input_dim + 1,
-                        self.embedding_dim,
-                        weights=[self.weights],
-                        input_length=self.max_sequence_len,
-                        trainable=True))
-            else:
-                model.add(Embedding(self.input_dim + 1,
-                        self.embedding_dim,
-                        input_length=self.max_sequence_len,
-                        trainable=True))
-            model.add(Dropout(self.dropout))
-            model.add(LSTM(self.lstm_dim))
-            model.add(Dropout(self.dropout))
-            model.add(Dense(self.output_dim, activation='softmax'))
-            model.compile(optimizer=self.optimizer, loss=self.loss,
-                  metrics=['accuracy'])
-            return model
+        self.attention_model = Model(inputs=inp, outputs=attention)
+        return model    
 
 class BiLSTM_Model(object):
 
@@ -131,49 +158,82 @@ class BiLSTM_Model(object):
             self.loss = 'categorical_crossentropy'
 
     def create_BiLSTM(self):
-        if self.attention:
-            inp = Input(shape=(self.max_sequence_len,))
-            if self.weights is not None:
-                emb = Embedding(self.input_dim + 1,
+        model = Sequential()
+        if self.weights is not None:
+            model.add(Embedding(self.input_dim + 1,
+                        self.embedding_dim,
+                        weights=[self.weights],
+                        input_length=self.max_sequence_len,
+                        trainable=True))
+        else:
+            model.add(Embedding(self.input_dim + 1,
+                        self.embedding_dim,
+                        input_length=self.max_sequence_len,
+                        trainable=True))
+        model.add(Dropout(self.dropout))
+        model.add(Bidirectional(LSTM(self.lstm_dim)))
+        model.add(Dropout(self.dropout))
+        model.add(Dense(self.output_dim, activation='softmax'))
+        model.compile(optimizer=self.optimizer, loss=self.loss,
+                  metrics=['accuracy'])
+        return model
+
+class BiLSTM_ATT_Model(object):
+
+    def __init__(self, vocab_size, embedding_dim, output_dim, weights=None, params=None):
+        self.input_dim = vocab_size
+        self.embedding_dim = embedding_dim
+        self.lstm_dim = self.embedding_dim
+        self.output_dim = output_dim
+        self.weights = weights
+        self.parse_params(params)
+        self.model = self.create_BiLSTM_ATT()
+
+    def parse_params(self, params=None):
+        if 'max_len' in params:
+            self.max_sequence_len = params['max_len']
+        else:
+            self.max_sequence_len = None
+        if 'dropout' in params:
+            self.dropout = params['dropout']
+        else:
+            self.dropout = .5
+        if 'trainable_embeddings' in params:
+            self.trainable = params['trainable_embeddings']
+        else:
+            self.trainable = True
+        if 'optimizer' in params:
+            self.optimizer = params['optimizer']
+        else:
+            self.optimizer = 'adam'
+        if 'loss' in params:
+            self.loss = params['loss']
+        else:
+            self.loss = 'categorical_crossentropy'
+
+    def create_BiLSTM_ATT(self):
+        inp = Input(shape=(self.max_sequence_len,))
+        if self.weights is not None:
+            emb = Embedding(self.input_dim + 1,
                         self.embedding_dim,
                         weights=[self.weights],
                         input_length=self.max_sequence_len,
                         trainable=True)(inp)
-            else:
-                emb = Embedding(self.input_dim + 1,
+        else:
+            emb = Embedding(self.input_dim + 1,
                         self.embedding_dim,
                         input_length=self.max_sequence_len,
                         trainable=True)(inp)
-            x = SpatialDropout1D(self.dropout)(emb)
-            x = Bidirectional(LSTM(self.lstm_dim, return_sequences=True))(x)
-            x, attention = Attention()(x)
-            x = Dense(self.output_dim, activation="sigmoid")(x)
-            model = Model(inputs=inp, outputs=x)
-            model.compile(loss=self.loss,
+        x = SpatialDropout1D(self.dropout)(emb)
+        x = Bidirectional(LSTM(self.lstm_dim, return_sequences=True))(x)
+        x, attention = Attention()(x)
+        x = Dense(self.output_dim, activation="sigmoid")(x)
+        model = Model(inputs=inp, outputs=x)
+        model.compile(loss=self.loss,
               optimizer=self.optimizer,
               metrics=['accuracy'])
-            self.attention_model = Model(inputs=inp, outputs=attention)
-            return model
-        else:
-            model = Sequential()
-            if self.weights is not None:
-                model.add(Embedding(self.input_dim + 1,
-                        self.embedding_dim,
-                        weights=[self.weights],
-                        input_length=self.max_sequence_len,
-                        trainable=True))
-            else:
-                model.add(Embedding(self.input_dim + 1,
-                        self.embedding_dim,
-                        input_length=self.max_sequence_len,
-                        trainable=True))
-            model.add(Dropout(self.dropout))
-            model.add(Bidirectional(LSTM(self.lstm_dim)))
-            model.add(Dropout(self.dropout))
-            model.add(Dense(self.output_dim, activation='softmax'))
-            model.compile(optimizer=self.optimizer, loss=self.loss,
-                  metrics=['accuracy'])
-            return model
+        self.attention_model = Model(inputs=inp, outputs=attention)
+        return model
 
 class CNN_Model(object):
 
